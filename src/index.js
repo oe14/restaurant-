ac9ad95efd713dbe1261f1b05a7d3a16161a9415
oe <yazidkandi4@gmail.com>
2025-02-13 22:22:54 +0100

    added btns && eventlistener for each

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-"console.log('Webpack is working!');" 
+import './style.css';
+
+console.log('Webpack is working!');
+const home = document.getElementById('home-btn');
+
+const menu = document.getElementById('menu-btn');
+
+const contact = document.getElementById('contact-btn');
+
+const content = document.getElementById("content");
+
+
+home.addEventListener("click", function(){
+content.innerHTML = "hello";
+
+})
+
+menu.addEventListener("click", function(){
+
+alert("hello world")
+
+})
+
+contact.addEventListener("click", function(){
+
+alert("hello world")
+
+})
